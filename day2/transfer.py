7f5af58e4c61793eca4d792fdbbdbb167042a34b
SK ARIAN ALI 11 <devarian69@gmail.com>
2026-05-18 00:01:48 +0530

    Update transfer.py
    Refactor loops to include else statements for clarity.

--- day2/transfer.py
+++ day2/transfer.py
@@ -4,9 +4,11 @@ if 'coding' in 'learn coding':
 for num in range(1, 11):
  if num % 2 == 0:
   continue # Skip even numbers
+ else:
   print(num)
   
 for num in range(1, 11):
  if num % 2 == 0:
   break # Exit the loop entirely
+ else:
   print(num) # Only '1' will be printed
